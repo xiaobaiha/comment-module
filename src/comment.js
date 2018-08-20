f60d963ad7a1f6b9db5b26040b7ac47cb73af8f2
CN\feizhijun <xiaobaihaha0001@163.com>
2018-08-20 10:40:56 +0800

    @dev add comment

--- src/comment.js
+++ src/comment.js
@@ -1,15 +1,33 @@
+/**
+ * 评论模块
+ * @class module:Comment
+ * 
+ */
 function Comment() {
-    var user;
-    var db;
-    var limit = 10;
-    var currentPage = 1;
-    var totalPage;
+    var user;   // 当前登录用户
+    var db;     // 数据库连接
+    var limit = 10;     // 每页数目
+    var currentPage = 1;// 当前页
+    var totalPage;      // 共多少页
+    /**
+     * 连接数据库 需引入db.js
+     * @private
+     * @method module:Comment#connectDB
+     * @returns {void}
+     */
     var connectDB = function () {
         if (!db) {
             db = new DBComment();
         }
         return db;
     };
+    /**
+     * 对Date格式化(yyyy-MM-dd HH:mm)
+     * @private
+     * @method module:Comment#formatDate
+     * @param {Date} date
+     * @returns {String} yyyy-MM-dd HH:mm格式化后的字符串
+     */
     var formatDate = function (date) {
         var d = new Date(date),
             hour = "" + d.getHours(),
@@ -25,6 +43,12 @@ function Comment() {
 
         return `${year}-${month}-${day} ${hour}:${minute}`;
     };
+    /**
+     * Comment模块初始化函数
+     * @public
+     * @method module:Comment#init
+     * @returns {void} 
+     */
     this.init = function () {
         db = connectDB();
         user = db.getLoginUser();
@@ -34,13 +58,26 @@ function Comment() {
         this.refreshPager();
         this.listen();
     };
+    /**
+     * 侦听处理数据库变化
+     * @public
+     * @method module:Comment#listen
+     * @returns {void} 
+     */
     this.listen = () => {
         db.on("listchange", event => {
             this.refreshPager();
         });
     };
+    /**
+     * 更新当前页评论列表
+     * @public
+     * @method module:Comment#refreshCommentList
+     * @returns {void} 
+     */
     this.refreshCommentList = () => {
         db.getCommentList({ page: currentPage, limit: limit }).then(list => {
+            // 评论列表为空时，跳转上一页
             if (list.length === 0) {
                 currentPage -= 1;
                 this.refreshPager();
@@ -54,6 +91,7 @@ function Comment() {
                 var publishDate = new Date(e.time);
                 var timeInteval = currentTime.getTime() - e.time;
                 var timeText;
+
                 if (timeInteval < 60 * 1000) {
                     timeText = Math.ceil(timeInteval / 1000) + "秒前";
                 } else if (timeInteval < 60 * 60 * 1000) {
@@ -82,29 +120,42 @@ function Comment() {
                 `;
                 docFragment.appendChild(li);
             });
-            commentList.innerHTML = "";
-            commentList.appendChild(docFragment);
+            commentList.innerHTML = ""; // 清空评论列表
+            commentList.appendChild(docFragment);   // 新评论列表内容
         });
     };
+    /**
+     * 页码跳转
+     * @public
+     * @method module:Comment#jumpPage
+     * @param {String} href 跳转地址
+     * @returns {void} 
+     */
     this.jumpPage = function (href) {
         if (/#\/page\/next/.test(href)) {
+            // 页码为最后一页，不跳页
             currentPage <= totalPage && (currentPage += 1);
         } else if (/#\/page\/previous/.test(href)) {
+            // 页码为第一页，不跳页
             currentPage > 1 && (currentPage -= 1);
-        } else {
+        } else if (/#\/page\/(\d+)/.test(href)) {
             currentPage = /#\/page\/(\d+)/.exec(href)[1] - 0; // page转成number
         }
-
         this.refreshPager();
     };
+    /**
+     * 更新分页器
+     * @public
+     * @method module:Comment#refreshPager
+     * @returns {void} 
+     */
     this.refreshPager = function () {
         db.getCommentTotal().then(function (total) {
             var commentList = document.querySelector("ul.m-pager");
             totalPage = Math.ceil(total / limit);
             var pageNumberList;
-            var totalComment = document.querySelector(".m-header .total_comment");
-
-            totalComment.textContent = total;
+            // 总评论数
+            document.querySelector(".m-header .total_comment").textContent = total;
             // 处理上/下一页是否可用
             commentList.querySelector("li.btn.prv").className =
                 currentPage === 1 ? "btn prv j-disabled" : "btn prv";
@@ -114,8 +165,8 @@ function Comment() {
             commentList.querySelectorAll("li.sep")[0].className =
                 totalPage <= 5 || currentPage <= 3 ? "sep hidden" : "sep";
             commentList.querySelectorAll("li.sep")[1].className =
-                totalPage <= 5 || currentPage >= totalPage - 2 ? "sep hidden" : "sep";
-
+                totalPage <= 5 || (currentPage >= totalPage - 2) ? "sep hidden" : "sep";
+            // 共显示5个页码，处理中间三个页码如何选择
             if (currentPage <= 4) {
                 pageNumberList = [1, 2, 3, 4, totalPage];
             } else if (currentPage >= totalPage - 3) {
@@ -135,6 +186,7 @@ function Comment() {
                     totalPage
                 ];
             }
+            // 根据页码数组更新DOM
             pageNumberList.forEach(function (page, index) {
                 var node = commentList.querySelectorAll("li.itm")[index];
                 node.className = page === currentPage ? "itm j-selected" : "itm";
@@ -143,14 +195,28 @@ function Comment() {
                 link.href = "#/page/" + page;
             });
         });
-        this.refreshCommentList();
+        this.refreshCommentList();  // 更新评论列表
     };
+    /**
+     * 增加评论，只操作数据库，数据变化由this.listen方法处理
+     * @public
+     * @method module:Comment#addComment
+     * @param {String} text 评论内容
+     * @returns {void} 
+     */
     this.addComment = function (text) {
         db.addComment({ id: "" + Date.now(), content: text }).then(function (data) {
                 console.log("add comment", data);
             }
         );
     };
+    /**
+     * 删除评论，只操作数据库，数据变化由this.listen方法处理
+     * @public
+     * @method module:Comment#deleteComment
+     * @param {String} id 被删除评论的id
+     * @returns {void} 
+     */
     this.deleteComment = function (id) {
         db.removeComment(id).then(function (data) {
             console.log("delete:", data);
--- src/record.md
+++ src/record.md
@@ -4,12 +4,12 @@
 
 ## 选择器优化
 
-### 层级原生选择器
+- 层级原生选择器  
 ```javascript
 var totalComment = document.getElementsByClassName("m-header")[0].getElementsByClassName("total_comment")[0];
 var userAvatar = document.getElementsByClassName("m-reply")[0].getElementsByClassName("w-avatar")[0].getElementsByTagName("img")[0];
 ```
-### querySelector
+- querySelector  
 ```javascript
 var totalComment = document.querySelector(".m-header .total_comment");
 var userAvatar = document.querySelector(".m-reply .w-avatar img");
@@ -17,12 +17,34 @@ var userAvatar = document.querySelector(".m-reply .w-avatar img");
 
 ## 列表dom操作优化
 
-### 循环插入
-### documentFragment
+- 循环插入  
+- documentFragment
 
 ## 删除评论合适刷新分页器
 
-### 总是刷新 
+- 总是刷新  
 多操作dom
-### 记录上一次总页面数目，与当前总数目不同时刷新
+- 记录上一次总页面数目，与当前总数目不同时刷新  
 多请求一次数据库
+
+## 分页器绑定点击事件
+
+- 绑定在可见页码以及上一页和下一页的按钮上   
+重复绑定多次相同事件
+- 绑定在分页器父级组件  
+判断不可点击字内容并处理
+```javascript
+// 省略号不进行跳页
+e.target.href && comment.jumpPage(e.target.href);
+```
+```javascript
+if (/#\/page\/next/.test(href)) {
+    // 页码为最后一页，不跳页
+    currentPage <= totalPage && (currentPage += 1);
+} else if (/#\/page\/previous/.test(href)) {
+    // 页码为第一页，不跳页
+    currentPage > 1 && (currentPage -= 1);
+} else {
+    currentPage = /#\/page\/(\d+)/.exec(href)[1] - 0; // page转成number
+}
+```
